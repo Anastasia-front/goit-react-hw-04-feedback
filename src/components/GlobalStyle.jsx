23a61bd464e7465c830ce308bc636d9176fd825a
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-04-19 18:53:59 +0300

    first changes

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  h1,h2,h3,h4,h5,h6,p {
+    margin: 0;
+  }
+
+body{padding:10px 0;}
+
+`;
---
+++ src/components/Notification/Notification.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Notific = styled.h1`
+  font-size: 30px;
+  margin-left: 40px;
+  margin-top: 20px;
+`;
